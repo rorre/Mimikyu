bfa6c338d5d44c4aaf264e38aaf812d47e08d553
Rendy Arya Kemal <renrror@gmail.com>
2025-01-21 15:06:11 +0700

    chore: remove express typing

--- types/express/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace Express {
-    export interface Response {
-        isError: boolean
-        isWrongStatus?: boolean
-        fileId?: string
-        isReauth?: boolean
-        delayTime: number
-    }
-}
